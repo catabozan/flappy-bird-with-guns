11997bd90fbbdae8986664d269866914725142bb
Catalin Bozan <catabozan@protonmail.com>
2022-05-28 12:53:44 +0300

    feat: collide function, new background, pipes

--- main.py
+++ main.py
@@ -9,15 +9,29 @@ pygame.init()
 
 window = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
-bg = pygame.image.load("flappy_bg.png")
-bird = pygame.image.load("bird_with_gun.png")
-bird = pygame.transform.scale(bird, (150, 150))
+bg = pygame.image.load("redbg.jpeg")
+bird_img = pygame.image.load("bird_with_gun.png")
+bird_img = pygame.transform.scale(bird_img, (150, 150))
+pipe_img = pygame.image.load("pipe.png")
+pipe_img = pygame.transform.scale(pipe_img, (120, 2100))
+
+
+def collide(image1, x1, y1, image2, x2, y2):
+    x = x2 - x1
+    y = y2 - y1
+    mask1 = pygame.mask.from_surface(image1)
+    mask2 = pygame.mask.from_surface(image2)
+    return mask1.overlap(mask2, (x, y)) != None
+
 
 y = 100
 vy = 0
 
 x = 100
 vx = 0
+
+min_pipe_y = -900
+max_pipe_x = -150
 
 clock = pygame.time.Clock()
 
@@ -26,19 +40,24 @@ while True:
 
     window.blit(bg, (0, 0))
 
+    window.blit(pipe_img, (300, max_pipe_x))
+
+    # if collide(bird_img, x, y, pipe_img, 300, max_pipe_x):
+    #     quit()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                vy = -20
+                vy = -10
 
-            if event.key == pygame.K_a:
-                vx = -5
-
-            if event.key == pygame.K_d:
-                vx = 5
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        vx = -10
+    if keys[pygame.K_d]:
+        vx = 10
 
     y = y + vy
     vy = vy + 1
@@ -49,6 +68,6 @@ while True:
     if vx > 0:
         vx = vx - 1
 
-    window.blit(bird, (x, y))
+    window.blit(bird_img, (x, y))
 
     pygame.display.update()
