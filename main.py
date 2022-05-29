2df3f52d940898d9ec43dfe7a762dd4e20f1e24b
Catalin Bozan <catabozan@protonmail.com>
2022-05-29 18:51:31 +0300

    refactor: create classes

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 # OLD GAME: https://pastebin.pl/view/6a5325c7
 import pygame
-
-# CONSTANTS
-DISPLAY_WIDTH = 1400
-DISPLAY_HEIGHT = 1050
+import random
 
 pygame.init()
-
-window = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-
-bg = pygame.image.load("redbg.jpeg")
-bird_img = pygame.image.load("ghost.png")
-bird_img = pygame.transform.scale(bird_img, (150, 150))
-pipe_img = pygame.image.load("pipe.png")
-pipe_img = pygame.transform.scale(pipe_img, (120, 2100))
 
 
 def collide(image1, x1, y1, image2, x2, y2):
@@ -24,55 +13,110 @@ def collide(image1, x1, y1, image2, x2, y2):
     return mask1.overlap(mask2, (x, y)) != None
 
 
-y = 100
-vy = 0
+class Game:
+    W_WIDTH = 1400
+    W_HEIGHT = 1050
+    W_BG = pygame.image.load("assets/redbg.jpeg")
+    CLOCK = pygame.time.Clock()
+    PIPE_SPRITE = pygame.image.load("assets/pipe.png")
+    PIPES_PER_SECOND = 1.0
+    pipes = []
 
-x = 100
-vx = 0
+    def __init__(self, player):
+        self.window = pygame.display.set_mode((self.W_WIDTH, self.W_HEIGHT))
+        self.player = player
+        self.createPipe()
 
-min_pipe_y = -900
-max_pipe_y = -150
-gap_y_offset = 930
-gap_x_size = 200
-gap_y_size = 120
+    def update(self):
+        self.CLOCK.tick(30)
+        self.window.blit(self.W_BG, (0, 0))
+        self.player.update(self.window)
+        for pipe in self.pipes:
+            pipe.update(self.window)
 
-clock = pygame.time.Clock()
+        # Events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.player.jump()
+
+        # Keys pressed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.player.moveLeft()
+        if keys[pygame.K_d]:
+            self.player.moveRight()
+
+    def createPipe(self):
+        pipe = Pipe(self.PIPE_SPRITE, self.W_WIDTH)
+        self.pipes.append(pipe)
+
+
+class Player:
+    Y_ACCELERATION = 10
+    X_ACCELERATION = 10
+    y = 100
+    vy = 0
+    x = 100
+    vx = 0
+    sprite = pygame.image.load("assets/ghost.png")
+
+    def __init__(self):
+        self.sprite = pygame.transform.scale(self.sprite, (150, 150))
+
+    def moveLeft(self):
+        self.vx = -1 * self.X_ACCELERATION
+
+    def moveRight(self):
+        self.vx = self.X_ACCELERATION
+
+    def jump(self):
+        self.vy = -1 * self.Y_ACCELERATION
+
+    def update(self, window):
+        # Move vertically
+        self.y = self.y + self.vy
+        self.vy = self.vy + 1
+
+        # Move horizontally
+        self.x = self.x + self.vx
+        if self.vx < 0:
+            self.vx = self.vx + 1
+        if self.vx > 0:
+            self.vx = self.vx - 1
+
+        # Draw player
+        window.blit(self.sprite, (self.x, self.y))
+
+
+class Pipe:
+    MIN_PIPE_Y = -900
+    MAX_PIPE_Y = -150
+    GAP_Y_OFFSET = 930
+    GAP_X_SIZE = 200
+    GAP_Y_SIZE = 120
+    X_ACCELERATION = -8
+
+    def __init__(self, sprite, x):
+        self.sprite = pygame.transform.scale(sprite, (120, 2100))
+        self.y = random.randrange(self.MIN_PIPE_Y, self.MAX_PIPE_Y, 50)
+        self.x = x
+
+    def update(self, window):
+        self.x = self.x + self.X_ACCELERATION
+        window.blit(self.sprite, (self.x, self.y))
+
+
+player = Player()
+game = Game(player)
 
 while True:
-    clock.tick(30)
 
-    window.blit(bg, (0, 0))
+    game.update()
 
-    window.blit(pipe_img, (300, max_pipe_y))
-    # pygame.draw.rect(window, (255, 255, 255),
-    #                  (300, max_pipe_y + gap_y_offset, gap_y_size, 200))
-
-    if collide(bird_img, x, y, pipe_img, 300, max_pipe_y):
-        quit()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                vy = -10
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        vx = -10
-    if keys[pygame.K_d]:
-        vx = 10
-
-    y = y + vy
-    vy = vy + 1
-
-    x = x + vx
-    if vx < 0:
-        vx = vx + 1
-    if vx > 0:
-        vx = vx - 1
-
-    window.blit(bird_img, (x, y))
+    # if collide(player.sprite, player.x, player.y, pipe_img, 300, max_pipe_y):
+    #     quit()
 
     pygame.display.update()
